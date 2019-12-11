7d2d43151ab6dd436341edd7215a3983d7ef8cd9
KalmikovSerg <kalmikovw13@gmailcom>
2019-12-11 16:02:28 -0600

    Initial Commit

---
+++ Project5_WordScrambler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5_WordScrambler
+//
+//  Created by SIARHEI KALMYKOU on 12/11/19.
+//  Copyright © 2019 SIARHEI KALMYKOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
